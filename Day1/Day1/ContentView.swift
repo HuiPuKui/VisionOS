88f6c91ade196dfaef096bf81de1cd89743dcf78
huipukui <huipukui@autohome.com.cn>
2024-05-23 10:55:59 +0800

    add Day1

---
+++ Day1/Day1/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Day1
+//
+//  Created by HuiPuKui on 2024/5/23.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, visionOS!")
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
